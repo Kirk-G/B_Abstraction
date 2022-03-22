4c8fa4aee131c9404fd49816f2fe15374d564103
Kirk_G <kdg432@shaw.ca>
2022-03-22 00:15:12 -0600

    Week 6 Video 3 Completed
    Added Health Component and Take Damage function

--- Source/B_Abstraction/Public/AbstractionPlayerCharacter.h
+++ Source/B_Abstraction/Public/AbstractionPlayerCharacter.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "HealthComponent.h"
 #include "AbstractionPlayerCharacter.generated.h"
+
+
 
 UCLASS()
 class B_ABSTRACTION_API AAbstractionPlayerCharacter : public ACharacter
@@ -15,11 +18,14 @@ public:
 	// Sets default values for this character's properties
 	AAbstractionPlayerCharacter();
 
-
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	void  OnDeath(bool IsFellOut);
+
+	UPROPERTY(EditAnywhere)
+	UHealthComponent* HealthComponent;
 
 public:	
 	// Called every frame
@@ -30,5 +36,7 @@ public:
 
 	// Called when the actor falls out of the world (below KILLZ)
 	virtual void FellOutOfWorld(const class UDamageType& dmgType) override;
+
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser);
 
 };
