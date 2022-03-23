7440efa8b04c1c1bfd76c3c6d67a56a99f36aec1
Kirk_G <kdg432@shaw.ca>
2022-03-22 22:41:07 -0600

    Video-4-Completed
    Created Deal Damage Component

---
+++ Source/B_Abstraction/Public/DealDamageComponent.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DealDamageComponent.generated.h"
+
+class UCapsuleComponent;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class B_ABSTRACTION_API UDealDamageComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDealDamageComponent();
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	float BaseDamage = 50.0f;
+
+	UPROPERTY(EditAnywhere, NoClear)
+	UCapsuleComponent* TriggerCapsule;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
